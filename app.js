9ac8d607da0d2935b13f92b955d0b534d3b25290
yimmer <yimmersamithduarteplata624@gmail.com>
2026-06-23 17:35:59 -0500

    completo la actividad de apropiacion

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// a. / → Mensaje de bienvenida al sistema.
+app.get('/', (req, res) => {
+    res.json({ message: '¡Bienvenido al Sistema de Gestión Académica!' });
+});
+
+// b. /aprendices → Mensaje descriptivo sobre la gestión de aprendices.
+app.get('/aprendices', (req, res) => {
+    res.json({ message: 'Portal de Gestión de Aprendices: Aquí podrás registrar, listar y administrar la información de los estudiantes.' });
+});
+
+// c. /programas → Mensaje descriptivo sobre los programas de formación.
+app.get('/programas', (req, res) => {
+    res.json({ message: 'Portal de Programas de Formación: Consulta aquí la oferta académica, diseños curriculares y fichas disponibles.' });
+});
+
+// 4. Configurar el servidor en el puerto 3000
+app.listen(PORT, () => {
+    console.log(`Servidor de Gestión Académica corriendo en http://localhost:${PORT}`);
+});
---
+++ readme.md
@@ -0,0 +1,21 @@
+## empiezo con la creacion basica de la estuctura 
+empiezo crear una carpeta de guia 
+luego verifico que se tenga una version del nodde para empezar a trabajar en el entorno
+se crea el archivo json
+agrego una carpeta node modules y luego la agrego en un archivo gitignore
+
+![alt text](image.png)
+
+## empezamos el trabajo principal del app.js
+creo la carpeta app.js para llevar a cabo los mensajes de respuesta
+creo las rutas principales donde van a venir las peticiones al backend 
+intento crear la manipulacion de mensajes agrgando como canal el json 
+
+![alt text](image-1.png)
+
+## se ejecuta el servidor
+con la funcion node app.js empieza a trabajar el servidor 
+![alt text](image.png)
+
+## vemos el servidor en ejecucion 
+![alt text](image.png)
